8d521c125b0f2183207172193e43ff6582071e7c
MuratMTM <murat.postmen@gmail.com>
2025-05-23 08:20:53 +0300

    Add fake weather api ex to learn promise in JS

---
+++ promiseWeather.js
@@ -0,0 +1,22 @@
+function getWeather(city) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const data = {
+                Istanbul: "🌤️ 21°C",
+                Berlin: "☁️ 17°C",
+                Tokyo: "🌧️ 23°C"
+            }
+
+            if (data[city]) {
+                resolve(`${city} hava durumu: ${data[city]}`)
+            }
+            else {
+                reject(`Şehire ait hava durumu bilgisi bulunamadı.`);
+            }
+        }, 2000);
+    });
+}
+
+getWeather(`London`)
+.then((result) => console.log(result))
+.catch((err) => console.log(`Error:`,err));
